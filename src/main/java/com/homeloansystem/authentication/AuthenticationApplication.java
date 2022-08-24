6b9b9270f167e9f1d06e8ac963d4bee51fa44ce0
wawdh01 <gaurav.wawdhane@gmail.com>
2022-08-24 15:02:09 +0530

    Changed Application.properties

--- src/main/java/com/homeloansystem/authentication/AuthenticationApplication.java
+++ src/main/java/com/homeloansystem/authentication/AuthenticationApplication.java
@@ -1,9 +1,11 @@
 package com.homeloansystem.authentication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
